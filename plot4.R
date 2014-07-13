02071cf12f0aba30781be728ad0b2bc893f61ce3
ashok <ashokjain001@gmail.com>
2014-07-13 15:19:37 -0500

    Plot 4
    updated Code

--- plot4.R
+++ plot4.R
@@ -1,15 +1,21 @@
-par(mfrow=c(2,2))
-plot(dt, subset$V3,xlab=" ", ylab= "Global Active Power", type= "l")
+plot2<-function(){
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile="./householdpower.zip",method="curl")
+    unzip(householdpower.zip)
+    power <- read.table("household_power_consumption.txt", comment.char= "", sep= ";", na.strings= "?", skip= 66600, n= 3000,stringsAsFactor=FALSE)
+    subdata <- subset(power, as.Date(V1) == '01/02/2007' | as.Date(V1) == '02/02/2007')   
+    date.time<-paste(subdata$V1,subdata$V2)
+    dt <- strptime(date.time, "%d/ %m/ %Y %H: %M: %S")
+    par(mfrow=c(2,2))
+    plot(dt, subset$V3,xlab=" ", ylab= "Global Active Power", type= "l")
 
-plot(dt, subset$V5,xlab="datetime", ylab= "Voltage", type= "l")
+    plot(dt, subset$V5,xlab="datetime", ylab= "Voltage", type= "l")
 
-with(subset,{
-    plot(dt,subset$V7,type="l",xlab=" ",ylab="Energy sub metering")
-    lines(dt,subset$V8,type="l",xlab=" ",ylab="Energy sub metering",col="red")
-    lines(dt,subset$V9,type="l",xlab=" ",ylab="Energy sub metering",col="blue")
+    with(subset,{
+        plot(dt,subset$V7,type="l",xlab=" ",ylab="Energy sub metering")
+        lines(dt,subset$V8,type="l",xlab=" ",ylab="Energy sub metering",col="red")
+        lines(dt,subset$V9,type="l",xlab=" ",ylab="Energy sub metering",col="blue")
 })
-legend("topright",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),text.font=1,lty=1,bty="n")
-
-plot(dt, subset$V4,xlab="datetime", ylab= "Global_reactive_power", type= "l")
-
-dev.copy(png,file="plot4.png")
+    legend("topright",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),text.font=1,lty=1,bty="n")
+    plot(dt, subset$V4,xlab="datetime", ylab= "Global_reactive_power", type= "l")
+    dev.copy(png,file="plot4.png")
+}
